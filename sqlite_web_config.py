a7337be1c0d8c9088075a4664f35e725c79b24f3
Elton Reis Lopes <115669349+EltonLopezzs@users.noreply.github.com>
2024-08-15 17:24:03 -0300

    Update sqlite_web_config.py

--- sqlite_web_config.py
+++ sqlite_web_config.py
@@ -1,7 +1,1 @@
-from passlib.hash import pbkdf2_sha256
 
-CONFIG = {
-    'db_filename': '/home/unialco/portaria/portaria.db',
-    'password_hash': pbkdf2_sha256.hash('u200809gx5'),
-    'password_salt': '12',
-}
